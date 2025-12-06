b1b13d042129763f763f0f7fa9210dbf72262b83
korakrit borisuth <captainza.055@gmail.com>
2025-12-06 18:25:14 +0700

    addflow1-2

--- Lab1/Lab1-1/lab1-1.c
+++ Lab1/Lab1-1/lab1-1.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
 int main() {
+    /* Variable Declaration */
     int n;
     int i;
+
+    /* Get User Input */
     scanf("%d", &n);
-    for (i = 1; i <= n; i++) {
+
+    /* Loop logic */
+    for (i = 1; i <= n; i++) 
+    {
         printf("[%d] Hello world\n", i);
     }
+
     return 0;
 }
