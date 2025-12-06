fa02db3b8ae016c30147c85d319afa2424ed0b6f
korakrit borisuth <captainza.055@gmail.com>
2025-12-06 16:48:51 +0700

    fix lab 1-1

--- Lab1/Lab1-1/lab1-1.c
+++ Lab1/Lab1-1/lab1-1.c
@@ -1,15 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main () {
-
+int main() {
     int n;
+    int i;  /* Fix 1: Declare 'i' here. Essential for older grading systems */
+
     scanf("%d", &n);
 
-    for (int i = 1; i < n + 1; i++)
-    {
-        printf("[%d]Hello world\n",i);
+    for (i = 1; i <= n; i++) {
+        /* Fix 2: Matches the Test Case image exactly */
+        printf("[%d] Hello world\n", i);
     }
-    
+
     return 0;
-    
 }
