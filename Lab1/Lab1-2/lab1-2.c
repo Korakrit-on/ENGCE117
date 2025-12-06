4efe155885ac43c36002b6a8f4c50023f8cef4c3
korakrit borisuth <captainza.055@gmail.com>
2025-12-06 19:22:51 +0700

    fix 1-3

--- Lab1/Lab1-2/lab1-2.c
+++ Lab1/Lab1-2/lab1-2.c
@@ -1,19 +1,35 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void GetSet(int a[], int *b){
+/* Function Definition */
+void GetSet(int a[], int *b) {
+    //declare loop variable at the top for strict compilers
+    int i; 
+
     printf("Enter number of elements: \n");
     scanf("%d", b);
+
+    //Get the numbers
+    // use *b to get the value stored at the pointer
     printf("Enter %d numbers: \n", *b);
-    for(int i = 0; i < *b; i++){
+    for (i = 0; i < *b; i++) {
         scanf("%d", &a[i]);
     }
 }
 
-int main(){
-    int data[100], num;
+int main() {
+    
+    int data[100];
+    int num;
+    int i;
+
+    /* Call function: Pass 'data' array and address of 'num' */
     GetSet(data, &num);
-    for(int i = 0; i < num; i++)
+
+    /* Print result to verify */
+    for (i = 0; i < num; i++) {
         printf("%d ", data[i]);
+    }
+    
     printf("\n");
     return 0;
 }
