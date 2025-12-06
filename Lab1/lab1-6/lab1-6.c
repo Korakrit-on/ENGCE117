f9c52dde92ff9d17dd5198ca33c064eed1fe1046
korakrit borisuth <captainza.055@gmail.com>
2025-12-06 21:39:31 +0700

    lab1-6 add

---
+++ Lab1/lab1-6/lab1-6.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+//Array
+int matrix[1000];
+
+/* * Function Definition: 
+ * Returns 'int' (status) instead of void.
+ * Uses 'int **value' to handle the address of the 'data' pointer.
+ */
+int GetMatrix(int **value, int *row, int *col) {
+    //Variables declared at the top
+    int i;
+    int j;
+
+    printf("How many row, col ? ");
+    scanf("%d %d", row, col);
+
+//Point the main 'data' variable to our global array 
+    *value = matrix;
+
+    //Nested Loop to get elements
+    for (i = 0; i < *row; i++) {
+        for (j = 0; j < *col; j++) {
+            // Prompt: a[0][0]
+            printf("a[%d][%d] = ", i, j);
+            
+            // Calculate index safely 
+            scanf("%d", &matrix[i * (*col) + j]);
+        }
+    }
+    
+    
+    return 1;//"Success"
+}
+
+int main() {
+    int *data, m, n;
+    int i, j; 
+
+    /* The main function ignores the return value, so 'int' works fine here */
+    GetMatrix(&data, &m, &n);
+
+    /* Print result for video verification */
+    printf("\nMatrix:\n");
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%d ", data[i * n + j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
