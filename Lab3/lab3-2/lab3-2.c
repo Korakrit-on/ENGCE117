907b26904b5dbee9ff460f29634c98284ef0b035
korakrit borisuth <captainza.055@gmail.com>
2025-12-28 19:43:42 +0700

    fix lab3-2

--- Lab3/lab3-2/lab3-2.c
+++ Lab3/lab3-2/lab3-2.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
 struct student {
-    char name[20];
+    char name[ 20 ];
     int age;
     char sex;
     float gpa;
 };
 
-//  Accept a pointer (add *) 
+// Function Definition
 void upgrade( struct student *child ) {
+    // Check if the student is Male ('M')
     if (child->sex == 'M' || child->sex == 'm') {
-        // Use arrow operator -> for pointers
-        child->gpa = child->gpa * 1.10; 
-    } 
+        // Increase GPA by 10% (Multiply by 1.10)
+        child->gpa = child->gpa * 1.10;
+    }
+    // Check if the student is Female ('F') - Assumption based on standard coding problems
     else if (child->sex == 'F' || child->sex == 'f') {
+        // Increase GPA by 20% (Multiply by 1.20)
         child->gpa = child->gpa * 1.20;
     }
 }
@@ -22,7 +25,8 @@ int main() {
     aboy.sex = 'M';
     aboy.gpa = 3.00;
     
-    // Pass the address of aboy (add &)
+    
+    // Because the function expects a pointer (*child), we must pass the address of aboy.
     upgrade( &aboy ); 
     
     printf( "%.2f", aboy.gpa );
